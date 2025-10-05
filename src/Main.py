3f77135df383a499ce2c234ccc90047843133e1b
DerfDa180155 <derflinger.david@gmail.com>
2025-10-05 23:46:45 +0200

    added code

---
+++ src/Main.py
@@ -0,0 +1,59 @@
+import pygame
+
+
+class main:
+    def __init__(self):
+        pygame.init()
+        pygame.display.init()
+
+        pygame.display.gl_set_attribute(pygame.GL_ACCELERATED_VISUAL, 0)
+        pygame.display.gl_set_attribute(pygame.GL_DOUBLEBUFFER, 1)
+
+        self.windowWidth = 1500
+        self.windowHeight = 1500
+
+        self.screen = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.RESIZABLE | pygame.GL_DOUBLEBUFFER)
+        pygame.display.set_caption("Powder Simulator by David Derflinger")
+
+        self.clock = pygame.time.Clock()
+        self.running = True
+
+        self.run()
+
+    def run(self):
+        oldMousePressed = pygame.mouse.get_pressed()
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: # Quit the Game
+                    self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE: # Quit the Game
+                        self.running = False
+
+            self.windowWidth = self.screen.get_width()
+            self.windowHeight = self.screen.get_height()
+
+            self.screen.fill((50, 50, 50))
+
+            mx, my = pygame.mouse.get_pos()
+            mousePressed = pygame.mouse.get_pressed()
+            mousePressedUp = []
+            mousePressedDown = []
+            for i in range(len(mousePressed)):
+                mousePressedUp.append(not mousePressed[i] and oldMousePressed[i])
+                mousePressedDown.append(mousePressed[i] and not oldMousePressed[i])
+
+            oldMousePressed = mousePressed
+
+
+
+
+
+
+
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    main()
