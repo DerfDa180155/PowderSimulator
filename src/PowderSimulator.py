c5ae4f0be99edfa191f1c5332c37c5f19b2b3beb
DerfDa180155 <derflinger.david@gmail.com>
2025-10-06 23:45:26 +0200

    updated code

---
+++ src/PowderSimulator.py
@@ -0,0 +1,12 @@
+
+
+class PowderSimulator:
+    def __init__(self, sizeX, sizeY):
+        self.sizeX = sizeX
+        self.sizeY = sizeY
+
+    def reset(self):
+        pass
+
+    def generateNext(self):
+        pass
