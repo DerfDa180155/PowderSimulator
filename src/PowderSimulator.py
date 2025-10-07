81b5cad9a4c56e2e17207ed163d48009b11117b1
DerfDa180155 <derflinger.david@gmail.com>
2025-10-07 23:43:01 +0200

    updated code

--- src/PowderSimulator.py
+++ src/PowderSimulator.py
@@ -4,9 +4,22 @@ class PowderSimulator:
     def __init__(self, sizeX, sizeY):
         self.sizeX = sizeX
         self.sizeY = sizeY
+        self.board = []
+
+        self.generateEmpty()
 
     def reset(self):
         pass
+
+    def generateEmpty(self):
+        self.board = []
+        temp = []
+
+        for x in range(self.sizeX):
+            temp.append([])
+
+        for y in range(self.sizeY):
+            self.board.append(temp)
 
     def generateNext(self):
         pass
