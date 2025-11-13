b697c92fb5e4f130260cf0b5f6663ac1d646a565
DerfDa180155 <derflinger.david@gmail.com>
2025-11-13 23:55:43 +0100

    updated code

---
+++ src/Sand.py
@@ -0,0 +1,13 @@
+import random
+
+import pygame
+
+class Sand:
+    def __init__(self):
+        self.colorArray = [(255,238,140), (228,217,111), (251,236,93), (240,220,130), (236,213,64)]
+
+        self.color = self.colorArray[random.randint(0, len(self.colorArray)-1)]
+
+
+    def next(self):
+        pass
