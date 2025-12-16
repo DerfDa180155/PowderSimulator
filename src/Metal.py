664adbfb57ad3d225f94c3ad69eb4c47868f0c87
DerfDa180155 <derflinger.david@gmail.com>
2025-12-16 23:57:15 +0100

    updated code

--- src/Metal.py
+++ src/Metal.py
@@ -4,7 +4,10 @@ import pygame
 
 class Metal:
     def __init__(self):
-        self.color = (125,125,125)
+        self.colorArray = [(128, 128, 128), (105, 105, 105), (120, 120, 125), (112, 128, 130), (90, 95, 95)]
+
+        self.color = self.colorArray[random.randint(0, len(self.colorArray) - 1)]
+
         self.weight = 99
 
     def next(self, board, newBoard, x, y):
