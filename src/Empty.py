d770827719f69c2b70ca701c842f995283d2c65f
DerfDa180155 <derflinger.david@gmail.com>
2026-01-15 23:56:51 +0100

    updated colors

--- src/Empty.py
+++ src/Empty.py
@@ -4,7 +4,7 @@ import pygame
 
 class Empty:
     def __init__(self):
-        self.color = (255,255,255)
+        self.color = (128,128,128)
         self.weight = 0
 
     def next(self, board, newBoard, x, y):
--- src/Metal.py
+++ src/Metal.py
@@ -4,7 +4,7 @@ import pygame
 
 class Metal:
     def __init__(self):
-        self.colorArray = [(128, 128, 128), (105, 105, 105), (120, 120, 125), (112, 128, 130), (90, 95, 95)]
+        self.colorArray = [(128,128,128), (105,105,105), (120,120,125), (112,128,130), (90,95,95)]
 
         self.color = self.colorArray[random.randint(0, len(self.colorArray) - 1)]
 
