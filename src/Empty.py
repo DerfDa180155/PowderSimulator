9ab6d66a0fd8c94937c886ad4ee7b19ebbeabf99
DerfDa180155 <derflinger.david@gmail.com>
2026-01-26 23:49:50 +0100

    updated empty color

--- src/Empty.py
+++ src/Empty.py
@@ -4,7 +4,7 @@ import pygame
 
 class Empty:
     def __init__(self):
-        self.color = (128,128,128)
+        self.color = (64,64,64)
         self.weight = -1
 
     def next(self, board, newBoard, x, y):
