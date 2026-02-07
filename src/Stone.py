90e365286aca607bf28672821ecc07ce74cd904e
DerfDa180155 <derflinger.david@gmail.com>
2026-02-07 23:53:04 +0100

    updated code

--- src/Stone.py
+++ src/Stone.py
@@ -11,4 +11,23 @@ class Stone:
         self.weight = 10
 
     def next(self, board, newBoard, x, y):
-        newBoard[y][x] = board[y][x]
+        if y > len(board) or y < 0 or x > len(board[y]) or x < 0:
+            return
+
+        moved = False
+
+        if y < len(board)-1:
+            if board[y+1][x].__class__ == Empty.Empty and newBoard[y+1][x].__class__ == Empty.Empty:
+                newBoard[y+1][x] = board[y][x]
+                moved = True
+
+            if board[y+1][x].weight < self.weight and newBoard[y+1][x].weight < self.weight and not moved:
+                temp = newBoard[y+1][x]
+                newBoard[y+1][x] = board[y][x]
+                newBoard[y][x] = temp
+                moved = True
+
+        if not moved:
+            newBoard[y][x] = board[y][x]
+
+        board[y][x] = Empty.Empty()
