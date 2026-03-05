daaf682c645b3e151f012036f1c4f254d87e3b21
DerfDa180155 <derflinger.david@gmail.com>
2026-03-05 23:54:53 +0100

    fixed a bug

--- src/PowderSimulator.py
+++ src/PowderSimulator.py
@@ -54,8 +54,8 @@ class PowderSimulator:
         if self.placeSize == 1:
             self.removeElement(x, y)
         else:
-            for i in range(len(self.board)):
-                for j in range(len(self.board[0])):
+            for i in range(len(self.board)+1):
+                for j in range(len(self.board[0])+1):
                     if (j - x) ** 2 + (i - y) ** 2 <= self.placeSize ** 2:
                         self.removeElement(j, i)
 
