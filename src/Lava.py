a89582e124734aa7cb0ebc10f92053b51f4ab07a
DerfDa180155 <derflinger.david@gmail.com>
2026-03-06 23:58:57 +0100

    added lava

---
+++ src/Lava.py
@@ -0,0 +1,14 @@
+import random
+import pygame
+import Empty
+
+class Lava:
+    def __init__(self):
+        self.colorArray = [(255,144,0)]
+
+        self.color = self.colorArray[random.randint(0, len(self.colorArray)-1)]
+
+        self.weight = 1
+
+    def next(self, board, newBoard, x, y, border):
+        board[y][x] = Empty.Empty()
